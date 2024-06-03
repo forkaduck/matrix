70cdb6134fac192fbd471e8430ff4fda99892a48
forkaduck <48600374+forkaduck@users.noreply.github.com>
2024-06-03 17:50:08 +0200

    Added helpers for test timming

--- src/vector/test.rs
+++ src/vector/test.rs
@@ -4,13 +4,17 @@ mod matrix_tests {
     use oorandom;
     use simplelog::{ColorChoice, Config, LevelFilter, TermLogger, TerminalMode};
     use std::path::PathBuf;
+    use std::time::Instant;
 
     use crate::loader::KernelLoader;
     use crate::Matrix;
     use matrix_macro::matrix_new;
 
-    #[test]
-    fn vec_ops() {
+    pub fn timer_end(start: Instant) {
+        println!("Time elapsed: {}s", (Instant::now() - start).as_secs_f64());
+    }
+
+    pub fn setup() -> Instant {
         TermLogger::init(
             LevelFilter::Debug,
             Config::default(),
@@ -19,17 +23,24 @@ mod matrix_tests {
         )
         .unwrap();
 
-        let mut loader = KernelLoader::new::<f16>(&PathBuf::from("./kernels")).unwrap();
-        loader.proque.set_dims(1 << 15);
+        Instant::now()
+    }
 
-        let mut one = matrix_new!(&loader, f16, 1, 10);
-        let mut two = matrix_new!(&loader, f16, 1, 10);
+    #[test]
+    fn vec_ops() {
+        let start = setup();
+
+        let mut loader = KernelLoader::new::<f32>(&PathBuf::from("./kernels")).unwrap();
+        loader.proque.set_dims(1 << 12);
+
+        let mut one = matrix_new!(&loader, f32, 1, 10);
+        let mut two = matrix_new!(&loader, f32, 1, 10);
 
         let mut rng = oorandom::Rand64::new(10);
 
         for _ in 0..10 {
-            one.A.push(rng.rand_float() as f16);
-            two.A.push(rng.rand_float() as f16);
+            one.A.push(rng.rand_float() as f32);
+            two.A.push(rng.rand_float() as f32);
         }
 
         info!("Input:");
@@ -63,5 +74,7 @@ mod matrix_tests {
         for i in 0..output.A.len() {
             assert_eq!(output.A[i] == one.A[i] / two.A[i], true);
         }
+
+        timer_end(start);
     }
 }
